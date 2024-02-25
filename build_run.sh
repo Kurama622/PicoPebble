37454a54d25f8c34b98a251065d18ea517888a25
StubbornVegeta <vegetaking@126.com>
2024-02-26 01:10:31 +0800

    update build_run.sh

--- build_run.sh
+++ build_run.sh
@@ -1,5 +1,14 @@
 node_num=$1
-cd build;
-make;
-mpirun -np $node_num ./example | tee mpi_run.txt;
-cd -
+if [ -d "build" ]; then
+    cd build;
+    make;
+    mpirun -np $node_num ./example | tee mpi_run.txt;
+    cd -
+else
+    mkdir build;
+    cd build;
+    cmake ..;
+    make;
+    mpirun -np $node_num ./example | tee mpi_run.txt;
+    cd -
+fi
