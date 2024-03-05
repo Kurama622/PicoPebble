3399a0949a3314a23ab5d04acbd010262f39c4cf
StubbornVegeta <vegetaking@126.com>
2024-03-05 21:47:34 +0800

    build: update cmakelist.txt and build_run.sh, set CMAKE_EXPORT_COMPILE_COMMANDS on, and allow mpi run as root

--- build_run.sh
+++ build_run.sh
@@ -2,13 +2,13 @@ node_num=$1
 if [ -d "build" ]; then
     cd build;
     make;
-    mpirun -np $node_num ./example | tee mpi_run.txt;
+    mpirun --allow-run-as-root -np $node_num ./example | tee mpi_run.txt;
     cd -
 else
     mkdir build;
     cd build;
     cmake ..;
     make;
-    mpirun -np $node_num ./example | tee mpi_run.txt;
+    mpirun --allow-run-as-root -np $node_num ./example | tee mpi_run.txt;
     cd -
 fi
